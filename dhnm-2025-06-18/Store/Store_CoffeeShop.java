e86826e9ed633fd5de2a50968b08392ae06f563b
Đinh Hữu Nhật Minh <yuumiyuumi2005@gmail.com>
2025-06-18 12:57:16 +0700

    Create Store_CoffeeShop.java

---
+++ dhnm-2025-06-18/Store/Store_CoffeeShop.java
@@ -0,0 +1,46 @@
+package Store;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import DATABASE.DatabaseConnection;
+import Menu.Products;
+
+public class Store_CoffeeShop {
+
+	private static final int maxProductsInMenu = 50;
+	private ArrayList<Products> itemsInStore = new ArrayList<>();
+	private int countMedia = 0;
+	
+	public ArrayList<Products> getItemsInStore(){
+		
+		return itemsInStore;
+	}
+	
+	public void loadProductsFromDB() {
+		itemsInStore.clear();
+        String query = "SELECT * FROM Products";
+        
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            
+            while (rs.next()) {
+                Products product = new Products(
+                    rs.getString("product_name"),
+                    rs.getFloat("price"),
+                    rs.getString("note"), // description
+                    rs.getString("category"),
+                    rs.getInt("stock")
+                );
+                product.setId(rs.getInt("product_id"));
+                itemsInStore.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
