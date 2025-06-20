df8e59b036b936834d70808af152bc8943331dbe
Đinh Hữu Nhật Minh <yuumiyuumi2005@gmail.com>
2025-06-20 13:55:51 +0700

    Update Customer.java

--- dhnm-2025-06-18/Customer/Customer.java
+++ dhnm-2025-06-18/Customer/Customer.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import Cart_CoffeeShop.Order_CoffeeShop;
 
 public class Customer {
+	private String customerId;
     private String name;
     private String phoneNumber;
     private String address;
+    private int bonusPoint;
     private ArrayList<Order_CoffeeShop> orderHistory;
 
     public Customer(String name, String phoneNumber, String address) {
@@ -14,6 +16,14 @@ public class Customer {
         this.phoneNumber = phoneNumber;
         this.address = address;
         this.orderHistory = new ArrayList<>();
+    }
+    
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
     }
 
     // Getter methods
@@ -27,6 +37,14 @@ public class Customer {
 
     public String getAddress() {
         return address;
+    }
+    
+    public int getBonusPoint() {
+        return bonusPoint;
+    }
+    
+    public void setBonusPoint(int bonusPoint) {
+        this.bonusPoint = bonusPoint;
     }
 
     public ArrayList<Order_CoffeeShop> getOrderHistory() {
