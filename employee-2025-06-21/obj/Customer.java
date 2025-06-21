97281448a869d7eb763a08081c68765b280aa8c5
Đinh Hữu Nhật Minh <yuumiyuumi2005@gmail.com>
2025-06-21 12:33:15 +0700

    Create Customer.java

---
+++ employee-2025-06-21/obj/Customer.java
@@ -0,0 +1,58 @@
+package obj;
+
+import java.util.ArrayList;
+
+public class Customer {
+	private String customerId;
+    private String name;
+    private String phoneNumber;
+    private String address;
+    private int bonusPoint;
+    private ArrayList<Order> orderHistory;
+
+    public Customer(String name, String phoneNumber, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.orderHistory = new ArrayList<>();
+    }
+    
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    
+    public int getBonusPoint() {
+        return bonusPoint;
+    }
+    
+    public void setBonusPoint(int bonusPoint) {
+        this.bonusPoint = bonusPoint;
+    }
+
+    public ArrayList<Order> getOrderHistory() {
+        return orderHistory;
+    }
+
+    // Method to add order to history
+    public void addOrder(Order order) {
+        orderHistory.add(order);
+    }
+
+}
